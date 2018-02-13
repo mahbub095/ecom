bedd3749ddb9cf6288dc677cfdf4f95654540967
mahbub095 <mahbub.095@gmail.com>
2018-02-13 21:29:10 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,135 @@
+$(document).ready(function(){
+	cat();
+	brand();
+	product();
+	function cat(){
+		$.ajax({
+			url	:	"action.php",
+			method:	"POST",
+			data	:	{category:1},
+			success	:	function(data){
+				$("#get_category").html(data);
+				
+			}
+		})
+	}
+	function brand(){
+		$.ajax({
+			url	:	"action.php",
+			method:	"POST",
+			data	:	{brand:1},
+			success	:	function(data){
+				$("#get_brand").html(data);
+			}
+		})
+	}
+		function product(){
+		$.ajax({
+			url	:	"action.php",
+			method:	"POST",
+			data	:	{getProduct:1},
+			success	:	function(data){
+				$("#get_product").html(data);
+			}
+		})
+	}
+	 
+ 
+	$("body").delegate(".category","click",function(event){
+		$("#get_product").html("<h3>Loading...</h3>");
+		event.preventDefault();
+		var cid = $(this).attr('cid');
+		
+			$.ajax({
+			url		:	"action.php",
+			method	:	"POST",
+			data	:	{get_seleted_Category:1,cat_id:cid},
+			success	:	function(data){
+				$("#get_product").html(data);
+				if($("body").width() < 480){
+					$("body").scrollTop(683);
+				}
+			}
+		})
+	
+	})
+	$("body").delegate(".selectBrand","click",function(event){
+		event.preventDefault();
+		$("#get_product").html("<h3>Loading...</h3>");
+		var bid = $(this).attr('bid');
+		
+			$.ajax({
+			url		:	"action.php",
+			method	:	"POST",
+			data	:	{selectBrand:1,brand_id:bid},
+			success	:	function(data){
+				$("#get_product").html(data);
+				if($("body").width() < 480){
+					$("body").scrollTop(683);
+				}
+			}
+		})
+	
+	})
+	$("#search_btn").click(function(){
+		$("#get_product").html("<h3>Loading...</h3>");
+		var keyword = $("#search").val();
+		if(keyword != ""){
+			$.ajax({
+			url		:	"action.php",
+			method	:	"POST",
+			data	:	{search:1,keyword:keyword},
+			success	:	function(data){ 
+				$("#get_product").html(data);
+				if($("body").width() < 480){
+					$("body").scrollTop(683);
+				}
+			}
+		})
+		}
+	})
+ 
+   
+	page();
+	function page(){
+		$.ajax({
+			url	:	"action.php",
+			method	:	"POST",
+			data	:	{page:1},
+			success	:	function(data){
+				$("#pageno").html(data);
+			}
+		})
+	}
+	$("body").delegate("#page","click",function(){
+		var pn = $(this).attr("page");
+		$.ajax({
+			url	:	"action.php",
+			method	:	"POST",
+			data	:	{getProduct:1,setPage:1,pageNumber:pn},
+			success	:	function(data){
+				$("#get_product").html(data);
+			}
+		})
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
