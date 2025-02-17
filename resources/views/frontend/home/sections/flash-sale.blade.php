badd4d997b38982a21d00964105a627626c335e3
mahbub095 <mahbub.095@gmail.com>
2025-02-17 16:53:05 +0600

    Flash sale

--- resources/views/frontend/home/sections/flash-sale.blade.php
+++ resources/views/frontend/home/sections/flash-sale.blade.php
@@ -1,66 +1,35 @@
-<!--============================
-        FLASH SELL START
-    ==============================-->
 <section id="wsus__flash_sell" class="wsus__flash_sell_2">
     <div class=" container">
         <div class="row">
             <div class="col-xl-12">
                 <div class="offer_time" style="background: url({{asset('frontend/images/flash_sell_bg.jpg')}})">
                     <div class="wsus__flash_coundown">
-                        <span class=" end_text">flash sell</span>
+                        <span class=" end_text">Flash Sale</span>
                         <div class="simply-countdown simply-countdown-one"></div>
-                        <a class="common_btn" href="{{route('flash-sale')}}">see more <i class="fas fa-caret-right"></i></a>
+                        <a class="common_btn" href="{{route('flash-sale')}}">see more <i
+                                class="fas fa-caret-right"></i></a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row flash_sell_slider">
-            @foreach ($flashSaleItems as $item)
-                @php
-                    $product = \App\Models\Product::find($item->product_id);
-                @endphp
 
-                <div class="col-xl-3 col-sm-6 col-lg-4">
-                    <div class="wsus__product_item">
-                        <span class="wsus__new">New</span>
-                        <span class="wsus__minus">-20%</span>
-                        <a class="wsus__pro_link" href="{{route('product-detail', $product->slug)}}">
-                            <img src="{{asset($product->thumb_image)}}" alt="product" class="img-fluid w-100 img_1"/>
-                            <img src=" @if(isset($product->productImageGalleries[0]->image))
-                            {{asset($product->productImageGalleries[0]->image)}}
-                        @else
-                            {{asset($product->thumb_image)}}
-                        @endif" alt="product" class="img-fluid w-100 img_2"/>
-                        </a>
-                        <ul class="wsus__single_pro_icon">
-                            <li><a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal-{{$product->id}}"><i
-                                        class="far fa-eye"></i></a></li>
-                            <li><a href="" class="add_to_wishlist" data-id="{{$product->id}}"><i class="far fa-heart"></i></a></li>
-                            <li><a href="#"><i class="far fa-random"></i></a>
-                        </ul>
-                        <div class="wsus__product_details">
-                            <a class="wsus__category" href="#">{{$product->category->name}}  </a>
-                            <p class="wsus__pro_rating">
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star-half-alt"></i>
-                                <span>(133 review)</span>
-                            </p>
-                            <a class="wsus__pro_name" href="#">{{($product->name)}}</a>
-                            <p class="wsus__price">{{$settings->currency_icon}}{{$product->offer_price}}
-                                <del>{{$settings->currency_icon}}{{$product->price}}</del>
-                            </p>
-                             <a class="add_cart" href="#">add to cart</a> 
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+
+          
 
         </div>
     </div>
 </section>
-<!--============================
-    FLASH SELL END
-==============================-->
+
+
+@push('scripts')
+    <script>
+        $(document).ready(function () {
+            simplyCountdown('.simply-countdown-one', {
+                year: {{date('Y', strtotime($flashSaleDate->end_date))}},
+                month: {{date('m', strtotime($flashSaleDate->end_date))}},
+                day: {{date('d', strtotime($flashSaleDate->end_date))}},
+            });
+        })
+    </script>
+@endpush
